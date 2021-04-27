9f4ddfb61412a512bf74057dd44068abe70e4d30
vijayasanthi-pv <vijayasanthi.pv@gmail.com>
2021-04-27 09:54:12 +0400

    Microservices prototype

--- comments/index.js
+++ comments/index.js
@@ -1,25 +1,66 @@
 const express  = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors'); //'Cors' enables communication between cross domains which is restricted by browser by default
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
-app.get('/posts/:id/comments', (req, res) => {
+app.get('/posts/:id/comments', async (req, res) => {
     res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
     const commentId = randomBytes(4).toString('hex');
     const comments = commentsByPostId[req.params.id] || [];
     const { content } = req.body;
 
-    comments.push({ id: commentId, content });
+    comments.push({ id: commentId, content, status: 'pending' });
     commentsByPostId[req.params.id] = comments;
+
+    await axios.post('http://localhost:4005/events',{
+        type:'CommentCreated',
+        data:{
+            id: commentId,
+            content,
+            postId: req.params.id,
+            status: 'pending'
+        }
+    });
     res.status(201).send(comments);
 
+});
+
+app.post('/events', async (req, res) => {
+    console.log('Event is received', req.body);
+
+    const { type, data } = req.body;
+
+    if (type === 'CommentModerated'){
+        const {id, postId, status, content } = data;
+        const comments = commentsByPostId[postId];
+
+        const comment = comments.find(comment => {
+            return comment.id === id;
+        });
+        comment.status = status;
+
+        await axios.post('http://localhost:4005/events',{
+            type : 'CommentUpdated',
+            data : {
+                id,
+                status,
+                postId,
+                content
+            }
+        });
+    }
+
+    res.send({});
 });
 
 app.listen(4001, () => {
--- posts/index.js
+++ posts/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const posts = {};
 
@@ -11,15 +14,31 @@ app.get('/posts', (req, res) => {
     res.send(posts);
 });
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
     const id = randomBytes(4).toString('hex');
     const { title } = req.body;
 
     posts[id] = { id, title };
 
+    //await axios.post('http://localhost:4005/events',{
+    await axios.post('http://event-bus-srv:4005/events',{ //Here 'event-bus-srv' is the name of the event bus clusterIP service in kubernetes 
+        type: 'PostCreated',
+        data:{
+            id,
+            title
+        }
+    });
     res.status(201).send(posts[id]);
 });
 
+app.post('/events', (req, res) => {
+    console.log('Received Event', req.body.type);
+
+    res.send({});
+});
+
 app.listen(4000, () => {
+    //console.log('v20');
+    console.log('v55');
     console.log('Listening on 4000');
 })
